151d3424faf0c338ef1c3a605e15160fe142ec23
DonLuyendijk <donluyendijk@gmail.com>
2019-09-03 13:05:31 +0200

    h

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#define filesize 688
 
 int main() {
 	printf("hello pizza!\n");
 	printf("deze repo bevat OK\n");
 	printf("bbbbbbbbbbbbb\n");
-	char hand[688];
+	char hand[filesize];
 	FILE *ascii;
 	ascii = fopen("ok.ascii", "r");
 	if(ascii == NULL) printf("cant open file");
-	for(int i = 0; i < 688; i++) {
+	for(int i = 0; i < filesize; i++) {
 		hand[i] = fgetc(ascii);
 	}
 	printf("%s\n", hand);
