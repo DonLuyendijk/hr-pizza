c23543c8386bb3ef187c5b385c0cadc71526068e
DonLuyendijk <donluyendijk@gmail.com>
2019-09-03 12:52:49 +0200

    now prints hand

--- main.c
+++ main.c
@@ -4,5 +4,13 @@ int main() {
 	printf("hello pizza!\n");
 	printf("deze repo bevat OK\n");
 	printf("bbbbbbbbbbbbb\n");
+	char hand[688];
+	FILE *ascii;
+	ascii = fopen("ok.ascii", "r");
+	if(ascii == NULL) printf("cant open file");
+	for(int i = 0; i < 688; i++) {
+		hand[i] = fgetc(ascii);
+	}
+	printf("%s\n", hand);
 	return 0;
 }
